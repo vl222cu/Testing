f32e75c74feae364097ae15e1a5a7624bcb8647a
Vivi <vivilam@hotmail.com>
2014-01-21 23:08:05 +0100

    Fler tester

--- Test/Test/default.aspx.cs
+++ Test/Test/default.aspx.cs
@@ -13,5 +13,20 @@ namespace Test
         {
 
         }
+
+        protected void SendButton_Click(object sender, EventArgs e)
+        {
+            var birthdate = DateTime.Parse(Birthdate.Text);
+            var nextBirthday = new DateTime(DateTime.Today.Year, birthdate.Month, birthdate.Day);
+            if (nextBirthday < DateTime.Today)
+            {
+                nextBirthday = nextBirthday.AddYears(1);
+            }
+
+            DayOfWeekLabel.Text = nextBirthday.ToString("dddd");
+            DaysUntilBirthdayLabel.Text = (nextBirthday - DateTime.Today).Days.ToString();
+            AgeOnLabel.Text = (nextBirthday.Year - birthdate.Year).ToString();
+            OutputPlaceHolder.Visible = true;
+        }
     }
 }
--- Test/Test/default.aspx.designer.cs
+++ Test/Test/default.aspx.designer.cs
@@ -22,21 +22,66 @@ namespace Test {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// TextBox1 control.
+        /// Label1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// Button1 control.
+        /// Birthdate control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.TextBox Birthdate;
+        
+        /// <summary>
+        /// SendButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SendButton;
+        
+        /// <summary>
+        /// OutputPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder OutputPlaceHolder;
+        
+        /// <summary>
+        /// DayOfWeekLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DayOfWeekLabel;
+        
+        /// <summary>
+        /// DaysUntilBirthdayLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DaysUntilBirthdayLabel;
+        
+        /// <summary>
+        /// AgeOnLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label AgeOnLabel;
     }
 }
